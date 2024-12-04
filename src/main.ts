00281096454da8a6bc2f666c4e7b1c55464794be
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-12-04 10:27:26 +0100

    chore: run basic http server

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+import http from 'node:http';
+
 async function main() {
-  console.log('Hello world !');
+  const httpServer = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.end('Hello World !');
+  });
+
+  httpServer.listen(3000, () => {
+    console.log(`\x1b[32mServer running on port 3000... \x1b[0m`);
+  });
 }
 
 main();
