003be120bc6321468ba166c908525958908ce5ad
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-12-26 10:07:00 +0100

    chore: load job with users

--- src/features/users/users.router.ts
+++ src/features/users/users.router.ts
@@ -5,11 +5,15 @@ import { authenticate } from '../auth/middlewares/auth.middleware';
 const usersRouter = Router();
 
 usersRouter.get('/', authenticate, async (req, res) => {
-  res.json({ users: await userRepository.find() });
+  res.json({ users: await userRepository.find({ relations: ['job'] }) });
 });
 
 usersRouter.get('/me', authenticate, async (req, res) => {
-  res.json(req.user);
+  const user = await userRepository.findOne({
+    where: { id: req.user.id },
+    relations: ['job'],
+  });
+  res.json(user);
 });
 
 export default usersRouter;
